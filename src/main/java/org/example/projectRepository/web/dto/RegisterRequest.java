1f6daac525ce61153b685453e446e12ffc457e2f
PoxTrox <simple@sofuni>
2025-02-02 22:30:42 +0200

    added front page register page and login page(again)

---
+++ src/main/java/org/example/projectRepository/web/dto/RegisterRequest.java
@@ -0,0 +1,22 @@
+package org.example.projectRepository.web.dto;
+
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class RegisterRequest {
+
+    @Size(min = 5 , max = 60, message = "Username must be at least 5 symbols")
+    private String username;
+
+    @Size(min = 5 , message = " Password must be at least 5 symbols")
+    private String password;
+
+}
